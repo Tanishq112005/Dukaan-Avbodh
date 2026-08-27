45172962940c9fc6a5d6737c5c3e8429c4555dd7
Tanishq112005 <tanishqjain1109@gmail.com>
2026-08-27 17:05:01 +0530

    update: folder structure is updated

--- backend/mcp/main.py
+++ backend/mcp/main.py
@@ -2,9 +2,9 @@ from mcp.server import mcp
 from config.database import db_connection
 import asyncio
 
-import mcp.product
-import mcp.order
-import mcp.user
+import product
+import order
+import user
 
 if __name__ == "__main__":
     asyncio.run(db_connection.init_db())   # tables create ho jaayengi agar exist nahi karti
