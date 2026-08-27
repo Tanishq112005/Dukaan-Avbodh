1954b681599902bc1d7675749b96373f4e84b9b6
Tanishq112005 <tanishqjain1109@gmail.com>
2026-08-27 17:15:24 +0530

    feat: check api's is ready

---
+++ backend/routes/checkout_routes.py
@@ -0,0 +1,19 @@
+# routes/checkout_routes.py
+from fastapi import APIRouter, Depends
+from controllers.checkout_controller import CheckoutController
+from schemas.checkout_schemas import CheckoutRequest
+from middleware.role_middleware import get_current_user, require_role
+from models.user import UserRole
+
+router = APIRouter(prefix="/checkout", tags=["Checkout"])
+controller = CheckoutController()
+
+
+async def checkout_route(
+    payload: CheckoutRequest,
+    current_user: dict = Depends(require_role(UserRole.CUSTOMER, UserRole.AI_AGENT))   # merchant checkout nahi karega
+):
+    return await controller.checkout(payload, current_user["user_id"])
+
+
+router.add_api_route("/", checkout_route, methods=["POST"])
