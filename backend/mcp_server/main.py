35a7f9886f6a2f3967836199caa6c069390042d5
Tanishq112005 <tanishqjain1109@gmail.com>
2026-08-29 02:09:24 +0530

    update: update the mcp_server/main.py file

--- backend/mcp_server/main.py
+++ backend/mcp_server/main.py
@@ -3,9 +3,13 @@ from mcp_server.server import mcp
 from config.database import db_connection
 import asyncio
 
-import mcp_server.product   # "product" ki jagah "mcp_server.product"
+import mcp_server.product
 import mcp_server.order
 import mcp_server.user
+
+# Naye imports add karo:
+import mcp_server.behavior
+import mcp_server.pricing
 
 if __name__ == "__main__":
     asyncio.run(db_connection.init_db())
