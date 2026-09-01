d620524c2791166ad488a049dcfcd09d7c969474
Tanishq112005 <tanishqjain1109@gmail.com>
2026-09-02 02:25:41 +0530

    update: repositories init.py file is updated

--- backend/repositories/__init__.py
+++ backend/repositories/__init__.py
@@ -2,13 +2,14 @@ from .user_repository import UserRepository
 from .product_repository import ProductRepository
 from .order_repository import OrderRepository
 from .discount_policy_repository import DiscountPolicyRepository
-from .cart_repository import CartRepository, cart_repository
+from .cart_write_repository import CartWriteRepository
+from .cart_repository import cart_repository
 
 __all__ = [
     "UserRepository",
     "ProductRepository",
     "OrderRepository",
     "DiscountPolicyRepository",
-    "CartRepository",
+    "CartWriteRepository",
     "cart_repository",
 ]
