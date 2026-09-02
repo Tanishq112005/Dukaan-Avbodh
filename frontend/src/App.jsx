09668ba36fc3aa083e7b62d682abfca313ed3c5f
Tanishq112005 <tanishqjain1109@gmail.com>
2026-09-02 20:02:52 +0530

    feat: until user not login , the AgentWidget is not showing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { Navbar } from "./features/shared/Navbar/Navbar";
 import { Footer } from "./features/shared/Footer/Footer";
 import { AgentWidget } from "./features/chatbot/ChatContainer";
@@ -9,29 +9,37 @@ import { CartPage } from "./features/cart/CartPage";
 import { LoginPage } from "./features/auth/LoginPage";
 import { SignupPage } from "./features/auth/SignupPage";
 import { MerchantDashboard } from "./features/merchant/MerchantDashboard";
-
 import { OrderConfirmedPage } from "./features/cart/OrderConfirmedPage";
+import { useStore } from "./store/useStore.js";
+
+function AppContent() {
+  const token = useStore((state) => state.token);
+
+  return (
+    <div className="min-h-screen flex flex-col relative bg-[#FBF8F3]">
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/products" element={<ProductListPage />} />
+          <Route path="/product/:id" element={<ProductDetailPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/order-confirmed" element={<OrderConfirmedPage />} />
+          <Route path="/merchant" element={<MerchantDashboard />} />
+        </Routes>
+      </main>
+      <Footer />
+      {token && <AgentWidget />}
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col relative bg-[#FBF8F3]">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/products" element={<ProductListPage />} />
-            <Route path="/product/:id" element={<ProductDetailPage />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/order-confirmed" element={<OrderConfirmedPage />} />
-            <Route path="/merchant" element={<MerchantDashboard />} />
-          </Routes>
-        </main>
-        <Footer />
-        <AgentWidget />
-      </div>
+      <AppContent />
     </Router>
   );
 }
